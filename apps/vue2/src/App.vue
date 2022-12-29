a0230135ca67b00d7c13d4860c3d608f5b1ef040
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-12-29 18:18:47 +0100

    build: repo init

---
+++ apps/vue2/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png"/>
+    <Particles id="tsparticles" :options="options" :particlesInit="particlesInit"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-property-decorator";
+import type { Engine } from "tsparticles-engine";
+import { loadFull } from "tsparticles";
+import { basic } from "tsparticles-demo-configs";
+
+import Particles from "@tsparticles/vue2";
+
+Vue.use(Particles);
+
+export default class App extends Vue {
+  options = basic;
+
+  async particlesInit(engine: Engine) {
+    await loadFull(engine);
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
