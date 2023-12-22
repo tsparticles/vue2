24a83a043c552dad4fa42ced026367698fe73da3
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-12-22 02:07:54 +0100

    feat: improving init function for v3

--- apps/vue2/src/App.vue
+++ apps/vue2/src/App.vue
@@ -1,22 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png"/>
-    <vue-particles id="tsparticles" :options="options" :particlesInit="particlesInit"/>
+    <vue-particles id="tsparticles" :options="options"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue } from "vue-property-decorator";
-import { type Engine } from "@tsparticles/engine";
-import { loadFull } from "tsparticles";
 import configs from "@tsparticles/configs";
 
 export default class App extends Vue {
   options = configs.basic;
-
-  async particlesInit(engine: Engine) {
-    await loadFull(engine);
-  }
 }
 </script>
 
